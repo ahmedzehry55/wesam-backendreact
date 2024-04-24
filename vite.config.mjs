945f3f38efda597d7b1ddc9d104ab7e1da0db11d
Ahmed Zehry <ahmedalhlwany2111@gmail.com>
2024-04-24 13:11:41 +0200

    ib15

--- vite.config.mjs
+++ vite.config.mjs
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => {
   process.env = { ...process.env, ...env }
 
   return {
-    base: '/admin/admin',
+    base: './',
     build: {
       outDir: 'build',
     },
